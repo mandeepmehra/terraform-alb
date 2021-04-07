142d1419cb4c4213e28c325919a721621b843f70
Mandeep Mehra <mandeep.mehra@xebia.com>
2021-04-06 23:58:10 -0700

    ALB WIP

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_alb" "name" {
 
 resource "aws_security_group" "albsg" {
   name   = "albsg"
-  vpc_id = "vpc-03f094db73a5298e9"
+  vpc_id = var.vpc_id
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
     from_port   = 80
@@ -24,3 +24,20 @@ resource "aws_security_group" "albsg" {
   }
 }
 
+resource "aws_lb_target_group" "myalbtg" {
+  name     = "myalb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_alb.name.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.myalbtg.arn
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "vpc_id" {
+  default = "vpc-03f094db73a5298e9"
+}
